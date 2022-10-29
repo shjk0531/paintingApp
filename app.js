2a8c7137c5d551625f8dedc4ea0ec314b11f784b
shjk <shinjk0531@gmail.com>
2022-10-30 04:47:22 +0900

    drawing human

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200)
-ctx.fillRect(400, 200, 50, 200)
-ctx.lineWidth = 2
-ctx.strokeRect(300, 300, 50, 100)
-ctx.fillRect(200, 200, 200, 20)
-ctx.moveTo(200, 200)
-ctx.lineTo(325, 100)
-ctx.lineTo(450, 200)
+ctx.fillRect(215, 150, 15, 100)
+ctx.fillRect(350, 150, 15, 100)
+ctx.fillRect(260, 150, 60, 200)
+
+ctx.arc(290, 100, 40, 0, 2 * Math.PI)
+ctx.fill()
+
+ctx.beginPath()
+ctx.fillStyle = "white"
+ctx.arc(305, 85, 5, Math.PI, 2 * Math.PI)
+ctx.arc(275, 85, 5, Math.PI, 2 * Math.PI)
 ctx.fill()
