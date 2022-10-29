b5069eb73e8172f487090859741effdf212aa97e
shjk <shinjk0531@gmail.com>
2022-10-30 04:25:20 +0900

    drawing house

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 100)
-ctx.rect(150, 150, 100, 100)
-ctx.rect(250, 250, 100, 100)
+ctx.fillRect(200, 200, 50, 200)
+ctx.fillRect(400, 200, 50, 200)
+ctx.lineWidth = 2
+ctx.strokeRect(300, 300, 50, 100)
+ctx.fillRect(200, 200, 200, 20)
+ctx.moveTo(200, 200)
+ctx.lineTo(325, 100)
+ctx.lineTo(450, 200)
 ctx.fill()
-
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100)
-ctx.fillStyle = "red"
-setTimeout(() => {
-    ctx.fill();
-}, 5000)
