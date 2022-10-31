e841a7414855617109b9bab91fc09c447b32a12f
shjk <shinjk0531@gmail.com>
2022-10-31 18:32:06 +0900

    Mouse Painting

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+ctx.lineWidth = 2
+
+const colors = [
+    "#ff3838",
+    "#ffb8b8",
+    "#c56cf0",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+]
+
+function onClick(event) {
+    ctx.beginPath()
+    ctx.moveTo(400,400)
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    ctx.strokeStyle = color
+    ctx.lineTo(event.offsetX, event.offsetY)
+    ctx.stroke()
+}
+
 canvas.width = 800;
 canvas.height = 800;
-
-ctx.fillRect(215, 150, 15, 100)
-ctx.fillRect(350, 150, 15, 100)
-ctx.fillRect(260, 150, 60, 200)
-
-ctx.arc(290, 100, 40, 0, 2 * Math.PI)
-ctx.fill()
-
-ctx.beginPath()
-ctx.fillStyle = "white"
-ctx.arc(305, 85, 5, Math.PI, 2 * Math.PI)
-ctx.arc(275, 85, 5, Math.PI, 2 * Math.PI)
-ctx.fill()
+canvas.addEventListener("mousemove", onClick)
