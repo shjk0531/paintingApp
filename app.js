961b1982e77461217a810d55d19924a99c671d8b
shjk0531 <shinjk0531@gmail.com>
2022-11-19 00:24:49 +0900

    Adding Image, Adding Text

--- app.js
+++ app.js
@@ -1,94 +1,121 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("line-width");
-const color =  document.getElementById("color")
-const colorOptions = Array.from(document.getElementsByClassName("color-option"))
-const modeBtn = document.getElementById("mode-btn")
-const destroyBtn = document.getElementById("destroy-btn")
-const eraserBtn = document.getElementById("eraser-btn")
+const color = document.getElementById("color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const fileInput = document.getElementById("file");
+const textInput = document.getElementById("text");
 
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 
-const CANVAS_WIDTH = 800
-const CANVAS_HEIGHT  = 800
-
-ctx.lineWidth = lineWidth.value
+ctx.lineWidth = lineWidth.value;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
-let isPainting  = false
-let isFilling   = false
+let isPainting = false;
+let isFilling = false;
 
-function onMove (event) {
-    if (isPainting) {
-        ctx.lineTo(event.offsetX, event.offsetY)
-        ctx.stroke()
-        return
-    }
-    ctx.beginPath()
-    ctx.moveTo(event.offsetX, event.offsetY)
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function startPainting () {
-    isPainting = true
+function startPainting() {
+  isPainting = true;
 }
 
 function cancelPainting() {
-    isPainting = false
+  isPainting = false;
 }
 
 function onLineWidthChange(event) {
-    ctx.lineWidth = event.target.value
+  ctx.lineWidth = event.target.value;
 }
 
 function onColorChange(event) {
-    ctx.strokeStyle = event.target.value
-    ctx.fillStyle = event.target.value
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
 }
 
 function onColorClick(event) {
-    const colorValue = event.target.dataset.color
-    ctx.strokeStyle = colorValue
-    ctx.fillStyle = colorValue
-    color.value = colorValue
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
 }
 
 function onModeClick() {
-    if(isFilling) {
-        isFilling = false
-        modeBtn.innerText = "Fill"
-    } else {
-        isFilling = true
-        modeBtn.innerText = "Draw"
-    }
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
 }
 
 function onCanvasClick() {
-    if(isFilling) {
-        ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
-    }
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
 }
 
 function onDestroyClick() {
-    ctx.fillStyle = "white"
-    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
-function onEraserClick () {
-    ctx.strokeStyle = "white"
-    isFilling = false 
-    modeBtn.innerText = "Fill"
+function onEraserClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
 }
 
-canvas.addEventListener("mousemove", onMove)
-canvas.addEventListener("mousedown", startPainting)
-canvas.addEventListener("mouseup", cancelPainting)
-canvas.addEventListener("mouseleave", cancelPainting)
-canvas.addEventListener("click", onCanvasClick)
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
+}
 
-lineWidth.addEventListener("change", onLineWidthChange)
-color.addEventListener("change", onColorChange)
-colorOptions.forEach(color => color.addEventListener("click", onColorClick))
+function onDoubleClick(event) {
+  const text = textInput.value;
+  if (text !== "") {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "50px serif";
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
+}
 
-modeBtn.addEventListener("click", onModeClick)
-destroyBtn.addEventListener("click", onDestroyClick)
-eraserBtn.addEventListener("click", onEraserClick)
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+canvas.addEventListener("dblclick", onDoubleClick);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
